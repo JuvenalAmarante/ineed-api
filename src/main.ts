0f6f9bc7c56b5307f74c9e228c16b3dd7869250e
Juvenal Amarante <juvenalamarante13@gmail.com>
2024-06-27 20:02:04 -0300

    feat: add error filters

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
@@ -16,6 +17,7 @@ async function bootstrap() {
 	app.enableCors();
 	app.enableShutdownHooks();
 	app.useGlobalPipes(new ValidationPipe({ transform: true }));
+	app.useGlobalFilters(new HttpExceptionFilter());
 
 	useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
