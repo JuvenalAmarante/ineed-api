a81939d5eeffa3183fe01109ce295dce6672a71a
Juvenal Amarante <juvenalamarante13@gmail.com>
2024-06-26 23:47:47 -0300

    refactor: update main structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule, {
+		cors: {
+			origin: ['*'],
+			credentials: true,
+		},
+	});
+
+	app.setGlobalPrefix('/api');
+
+	app.enableCors();
+	app.enableShutdownHooks();
+	app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+	useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
