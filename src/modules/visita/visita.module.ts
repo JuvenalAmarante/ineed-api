fa875b46890427583f296ce3cca470ac92834985
Juvenal Amarante <juvenalamarante13@gmail.com>
2024-07-19 14:36:45 -0300

    fix: add imports on visit module

--- src/modules/visita/visita.module.ts
+++ src/modules/visita/visita.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VisitaService } from './visita.service';
 import { VisitaController } from './visita.controller';
+import { MailModule } from 'src/shared/services/mail/mail.module';
+import { SmsModule } from 'src/shared/services/sms/sms.module';
+import { PushNotificationModule } from 'src/shared/services/push-notification/push-notification.module';
 
 @Module({
+  imports: [MailModule, SmsModule, PushNotificationModule],
   controllers: [VisitaController],
   providers: [VisitaService],
 })
